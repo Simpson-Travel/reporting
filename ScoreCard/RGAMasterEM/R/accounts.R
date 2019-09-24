18908487172a9c384722565b11128350419a1f0a
EuanMack <euanm@simpsontravel.com>
2019-09-24 10:55:42 +0100

    Removed RGA build file

--- ScoreCard/RGAMasterEM/R/accounts.R
+++
@@ -1,7 +0,0 @@
-#' @template list_accounts
-#' @include mgmt.R
-#' @export
-list_accounts = function(start.index = NULL, max.results = NULL, token) {
-    path <- "management/accounts"
-    list_mgmt(path, list(start.index = start.index, max.results = max.results), token)
-}
--- ScoreCard/RGAMasterEM/inst/shiny-examples/01-dimsmets/global.R
+++
@@ -1,6 +0,0 @@
-ga <- try(RGA::list_dimsmets("ga"), silent = TRUE)
-if (inherits(ga, "try-error"))
-    ga <- RGA::ga
-
-cn <- colnames(ga)
-selected <- c("id", "uiName", "type", "description")
