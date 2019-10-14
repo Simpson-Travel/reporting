a7e8df69510cc8fb3da40286d2334cc499230aaa
EuanMack <euanm@simpsontravel.com>
2019-10-14 16:03:03 +0100

    Score Card for next year departures
    Added a very basic means to cope with next year departures

--- ScoreCard/ScoreCard-Email-Values-OMT.r
+++ ScoreCard/ScoreCard-Email-Values-OMT.r
@@ -1,12 +1,14 @@
 library(RDCOMClient)
 
-reportValues <- paste("V:/Shared Documents/ScoreCard/ReportValues/ScoreCard-",as.Date(Sys.Date(),format='%Y%m%d'),".xlsx",sep="")
+reportValues.TY <- paste("V:/Shared Documents/ScoreCard/ReportValues/ScoreCard-",format(Sys.Date(),'%Y'),"Departures-",as.Date(Sys.Date(),format='%Y%m%d'),".xlsx",sep="")
+reportValues.NY <- paste("V:/Shared Documents/ScoreCard/ReportValues/ScoreCard-",format(Sys.Date()+365,'%Y'),"Departures-",as.Date(Sys.Date(),format='%Y%m%d'),".xlsx",sep="")
 
 OutApp <- COMCreate("Outlook.Application")
 
 outMail = OutApp$CreateItem(0)
-outMail[["To"]] = paste("euanm@simpsontravel.com", sep=";", collapse=NULL)
-outMail[["Bcc"]] = paste("euanm@simpsontravel.com", sep=";", collapse=NULL)
+outMail[["SentOnBehalfOfName"]] = "data.warehouse@simpsontravel.com"
+outMail[["To"]] = paste("OMT@simpsontravel.com", sep=";", collapse=NULL)
+outMail[["Bcc"]] = paste("euanm@simpsontravel.com","dave.thomas@clickvisionmedia.co.uk", sep=";", collapse=NULL)
 outMail[["subject"]] = paste("Score Card - ",as.Date(Sys.Date(),format='%Y%m%d'))
 outMail[["HTMLbody"]]="<p>Hi all,</p>
 <p>Please find attached the latest version of the score card.</p>
@@ -19,5 +21,6 @@ euanm@simpsontravel.com<br />
 <a href='https://www.simpsontravel.com'>simpsontravel.com</a><br />
 <p>The Grange, Bank of England Sports Ground, Bank Lane, London, SW15 5JT<br />
 <img src='https://simpsontravel.blob.core.windows.net/media/26264/emailfooter.png'></p>"
-outMail[["attachments"]]$Add(reportValues)
+outMail[["attachments"]]$Add(reportValues.TY)
+outMail[["attachments"]]$Add(reportValues.NY)
 outMail$Send()
